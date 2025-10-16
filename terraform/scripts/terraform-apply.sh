6f87dd3e039057e1d931385cda2425ef79aaa4b3
Andy Wood <andy@Vimes.local>
2025-10-16 15:41:47 +0100

    feat: wrapper to terraform apply

---
+++ terraform/scripts/terraform-apply.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+function fn_usage() {
+  echo
+  echo "Apply the Terraform plan. No arguments - configuration is driven by environment"
+  echo "variables."
+  echo
+  echo "Usage:"
+  echo "  $0 [-h]"
+  echo
+  echo -e "${BOLD}IMPORTANT${RESET} The script checks that these environment variables are defined:"
+  echo -e "  ${BOLD}TERRAFORM_PLAN_FILE${RESET}  Terraform plan, e.g. \"plan.cache\""
+  echo
+  echo "The optional \"TF_EXTRA_VARS\" environment variable is passed into the command."
+  echo
+  echo "The following environment variables - if defined - will manage the outputs"
+  echo "of this script:"
+  echo "  TERRAFORM_CURRENT_STATE - Human readable file containing current Terraform state"
+  echo
+}
+
+function fn_fail_if_missing() {
+  local message=$1
+  local string=$2
+  if [[ -z "${string}" ]]
+  then
+    echo -e "${RED}ERROR:${RESET} ${message}"
+    exit 1
+  fi
+}
+
+##### ENTRY
+AWS_REGION="eu-west-2"
+
+BOLD="\033[1m"
+RED="\033[1;31m"
+RESET="\033[0m"
+
+while getopts "h" opt; do
+  case "${opt}" in
+  h | \?)
+    fn_usage
+    exit 0
+    ;;
+  [?])
+    shift
+    ;;
+  esac
+done
+
+fn_fail_if_missing "${BOLD}TERRAFORM_PLAN_FILE${RESET} is not defined" "${TERRAFORM_PLAN_FILE:-}"
+
+# shellcheck disable=SC2086
+terraform apply -auto-approve ${TERRAFORM_PLAN_FILE}
+
+#------------------------------------------------------------------------------
+# Artifacts
+if [[ -n "${TERRAFORM_CURRENT_STATE:-}" ]]
+then
+  # Full state in human readable form
+  printf %80s ' ' | tr ' ' '-'
+  printf "\n"
+  echo "Copying state into ${TERRAFORM_CURRENT_STATE}"
+  terraform show -no-color > "${TERRAFORM_CURRENT_STATE}"
+fi
