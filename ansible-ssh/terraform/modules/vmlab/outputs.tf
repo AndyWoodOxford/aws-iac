f4ee1de086f013b48e245f5dfdc905ca3e231dd6
Andy Wood <andy@Vimes.local>
2025-10-29 11:41:28 +0000

    feat: output public ip

--- ansible-ssh/terraform/modules/vmlab/outputs.tf
+++ ansible-ssh/terraform/modules/vmlab/outputs.tf
@@ -1,6 +1,6 @@
 output "instances_ipv4" {
   description = "IPV4 addresses of the instance(s)"
-  value       = aws_instance.vm[*].private_ip
+  value       = aws_instance.vm[*].public_ip
 }
 
 output "nat_gateway" {
